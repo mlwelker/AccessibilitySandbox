fcee2cae3c3d6ae639a6b1ae74bdc465a69312e6
mlwelker <nutria.watts.0g@icloud.com>
2023-09-16 15:30:44 -0500

    accessibility value controls modifiers

--- AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/ContentView.swift
@@ -2,15 +2,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 10
+    
     var body: some View {
         VStack {
-            Text("Your score is")
-            Text("1000")
-                .font(.title)
+            Text("Value: \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .accessibilityElement(children: .ignore)
-        .accessibilityLabel("Your score is 1000")
-//        .accessibilityElement(children: .combine)
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+        }
     }
 }
 
