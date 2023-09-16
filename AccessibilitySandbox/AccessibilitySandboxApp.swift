2bc62a034ec37d5e42f4fd0a614d1ad557d05b48
mlwelker <nutria.watts.0g@icloud.com>
2023-09-16 13:45:30 -0500

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Michael Welker on 2023-09-16.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
